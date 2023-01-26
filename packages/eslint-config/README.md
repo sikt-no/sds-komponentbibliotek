a5780ca61d436c7df8d5a82358ee9990d8549386
Kristoffer Nordström <totte.nordstrom@gmail.com>
2023-01-26 16:22:59 +0100

    feat(eslint-config): add package

---
+++ packages/eslint-config/README.md
@@ -0,0 +1,17 @@
+# `@sikt/eslint-config-sds`
+
+## Consume
+
+```sh
+npm i -s @sikt/eslint-config-sds
+```
+
+### Config
+
+`.eslintrc.js`
+
+```json
+"eslintConfig": {
+  "extends": ["@sikt/eslint-config-sds"]
+}
+```
--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -7,17 +7,10 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
     "plugin:jsx-a11y/recommended",
     "prettier",
   ],
-  overrides: [],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react", "@typescript-eslint", "jsx-a11y"],
+  plugins: ["react", "jsx-a11y"],
   rules: {
     "jsx-a11y/anchor-ambiguous-text": [
       "error",
