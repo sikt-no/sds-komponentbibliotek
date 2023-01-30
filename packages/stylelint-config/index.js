39e1c3367d6e3c9317b17955fd96d1246453371d
Kristoffer Nordström <totte.nordstrom@gmail.com>
2023-01-30 08:12:03 +0100

    chore: stylelint rules

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -1,6 +1,13 @@
 module.exports = {
   extends: ["stylelint-config-standard", "stylelint-config-prettier"],
   rules: {
+    "media-feature-name-disallowed-list": [
+      "^max-width",
+      {
+        message: "Use min-width for a mobile-first approach",
+      },
+    ],
+    "selector-max-type": 0,
     "selector-class-pattern": [
       "^(?<block>(?:[a-z][a-z0-9]*)(?:-[a-z0-9]+)*)(?<element>(?:__[a-z][a-z0-9]*(?:-[a-z0-9]+)*))?(?<modifier>(?:--[a-z][a-z0-9]*)(?:-[a-z0-9]+)*)?$",
       {
--- stylelint.config.js
+++ stylelint.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   extends: "./packages/stylelint-config",
+  rules: {
+    "selector-class-pattern": [
+      "^sds-(?<block>(?:[a-z][a-z0-9]*)(?:-[a-z0-9]+)*)(?<element>(?:__[a-z][a-z0-9]*(?:-[a-z0-9]+)*))?(?<modifier>(?:--[a-z][a-z0-9]*)(?:-[a-z0-9]+)*)?$",
+      {
+        resolveNestedSelectors: true,
+      },
+    ],
+  },
 };
