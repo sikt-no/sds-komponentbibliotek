84110dbecedae23d7e09aeb02f908bf45c8908ad
Kristoffer Nordström <totte.nordstrom@gmail.com>
2023-03-31 14:24:58 +0200

    feat(gatsby): change link for retningslinjer & use new components

--- gatsby/src/components/Footer.tsx
+++ gatsby/src/components/Footer.tsx
@@ -1,50 +1,43 @@
 import React from "react";
 import { Link, Heading2 } from "@sikt/sds-core";
-import { SecondaryLogo } from "@sikt/sds-logo";
 import { UnorderedList, ListItem } from "@sikt/sds-list";
+import { Footer as SdsFooter } from "@sikt/sds-footer";
 import * as style from "./footer.module.css";
 
 const Footer = () => {
   return (
-    <footer className={style.sdsSiktFooter} data-color-scheme="dark">
-      <div className={style.sdsSiktFooter__content}>
-        <div>
-          <Link href="//sikt.no" className={style.sdsSiktFooter__logoLink}>
-            <SecondaryLogo />
-          </Link>
-        </div>
-        <div>
-          <Heading2
-            headingType="overline"
-            className={style.sdsSiktFooter__linkHeading}
-          >
-            Kontakt oss
-          </Heading2>
-          <UnorderedList>
-            <ListItem>
-              Slack{" "}
-              <Link
-                href="https://sikt-no.slack.com/archives/C04K82KES0J"
-                target="_blank"
-                aria-label="Slack #komponenter"
-              >
-                #komponenter
-              </Link>
-            </ListItem>
-            <ListItem>
-              Teams{" "}
-              <Link
-                href="https://teams.microsoft.com/l/channel/19%3a82f3200166fe4d478fcbbdf601156a04%40thread.tacv2/Designsystem?groupId=df522351-7400-437c-8afb-110611f841fa&tenantId=cb342146-7965-425e-83ab-88f8ecb99e0d"
-                target="_blank"
-                aria-label="Teams Designsystem"
-              >
-                Designsystem
-              </Link>
-            </ListItem>
-          </UnorderedList>
-        </div>
+    <SdsFooter>
+      <div>
+        <Heading2
+          headingType="overline"
+          className={style.sdsSiktFooter__linkHeading}
+        >
+          Kontakt oss
+        </Heading2>
+        <UnorderedList>
+          <ListItem>
+            Slack{" "}
+            <Link
+              href="https://sikt-no.slack.com/archives/C04K82KES0J"
+              target="_blank"
+              aria-label="Slack #komponenter"
+            >
+              #komponenter
+            </Link>
+          </ListItem>
+          <ListItem>
+            Teams{" "}
+            <Link
+              href="https://teams.microsoft.com/l/channel/19%3a82f3200166fe4d478fcbbdf601156a04%40thread.tacv2/Designsystem?groupId=df522351-7400-437c-8afb-110611f841fa&tenantId=cb342146-7965-425e-83ab-88f8ecb99e0d"
+              target="_blank"
+              aria-label="Teams Designsystem"
+            >
+              Designsystem
+            </Link>
+          </ListItem>
+        </UnorderedList>
       </div>
-    </footer>
+    </SdsFooter>
   );
 };
 
--- gatsby/src/components/Header.tsx
+++ gatsby/src/components/Header.tsx
@@ -1,41 +1,34 @@
 import React from "react";
-import { Link, ScreenReaderOnly } from "@sikt/sds-core";
-import { PrimaryLogo } from "@sikt/sds-logo";
+import { Header as SdsHeader } from "@sikt/sds-header";
 import * as style from "./header.module.css";
 
 const Header = () => {
   return (
-    <>
-      <ScreenReaderOnly isFocusable className={style.sdsSiktHeader__skipLink}>
-        <Link href="#main">Gå til innhold</Link>
-      </ScreenReaderOnly>
-      <header className={`${style.sdsSiktHeader} sds-sikt-header`}>
-        <PrimaryLogo className={style.sdsSiktHeader__logo} />
-        <nav
-          aria-label="Navigajon"
-          className={`sds-tabs ${style.sdsSiktHeader__tab}`}
-        >
-          <ul className={`sds-tabs__tab-list ${style.sdsSiktHeader__tabList}`}>
-            <li className={style.sdsSiktHeader__tabItem}>
-              <a
-                href="/retningslinjer/"
-                className={`sds-tabs__tab ${style.sdsSiktHeader__tabLink}`}
-              >
-                Retningslinjer
-              </a>
-            </li>
-            <li className={style.sdsSiktHeader__tabItem}>
-              <a
-                href="/komponenter/"
-                className={`sds-tabs__tab ${style.sdsSiktHeader__tabLink}`}
-              >
-                Komponenter
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </>
+    <SdsHeader logoText="designsystem" className={style.sdsSiktHeader}>
+      <nav
+        aria-label="Navigajon"
+        className={`sds-tabs ${style.sdsSiktHeader__tab}`}
+      >
+        <ul className={`sds-tabs__tab-list ${style.sdsSiktHeader__tabList}`}>
+          <li className={style.sdsSiktHeader__tabItem}>
+            <a
+              href="/retningslinjer/"
+              className={`sds-tabs__tab ${style.sdsSiktHeader__tabLink}`}
+            >
+              Retningslinjer
+            </a>
+          </li>
+          <li className={style.sdsSiktHeader__tabItem}>
+            <a
+              href="/komponenter/"
+              className={`sds-tabs__tab ${style.sdsSiktHeader__tabLink}`}
+            >
+              Komponenter
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </SdsHeader>
   );
 };
 
