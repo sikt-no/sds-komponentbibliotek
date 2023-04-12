0fbeb8b4fe5db7bc3805b668adfc20162e479904
Kristoffer Nordström <totte.nordstrom@gmail.com>
2023-04-12 16:29:43 +0200

    refactor(gatsby): use tab link component in header

--- gatsby/src/components/Header.tsx
+++ gatsby/src/components/Header.tsx
@@ -1,30 +1,18 @@
 import React from "react";
 import { Header as SdsHeader } from "@sikt/sds-header";
+import { TabLink } from "@sikt/sds-tabs";
 import * as style from "./header.module.css";
 
 const Header = () => {
   return (
     <SdsHeader logoText="designsystem" className={style.sdsSiktHeader}>
-      <nav
-        aria-label="Navigajon"
-        className={`sds-tabs ${style.sdsSiktHeader__tab}`}
-      >
+      <nav aria-label="Navigajon" className="sds-tabs">
         <ul className={`sds-tabs__tab-list ${style.sdsSiktHeader__tabList}`}>
-          <li className={style.sdsSiktHeader__tabItem}>
-            <a
-              href="/retningslinjer/"
-              className={`sds-tabs__tab ${style.sdsSiktHeader__tabLink}`}
-            >
-              Retningslinjer
-            </a>
+          <li>
+            <TabLink href="/retningslinjer/">Retningslinjer</TabLink>
           </li>
-          <li className={style.sdsSiktHeader__tabItem}>
-            <a
-              href="/komponenter/"
-              className={`sds-tabs__tab ${style.sdsSiktHeader__tabLink}`}
-            >
-              Komponenter
-            </a>
+          <li>
+            <TabLink href="/komponenter/">Komponenter</TabLink>
           </li>
         </ul>
       </nav>
