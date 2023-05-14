b4cd018a29ee4a051bb333cde902a7ac07436688
designsystem-renovatebot <group3766_bot@noreply.gitlab.sikt.no>
2023-05-14 00:07:03 +0000

    chore(deps): update dependency @svgr/cli to v8

--- packages/icons/svgr/indexTemplate.js
+++ packages/icons/svgr/indexTemplate.js
@@ -1,15 +1,15 @@
 const path = require("path");
 
 function defaultIndexTemplate(filePaths) {
-  const exportEntries = filePaths.map((filePath) => {
+  const exportEntries = filePaths.map(({ path: filePath }) => {
     const basename = path.basename(filePath, path.extname(filePath));
     const exportName = /^\d/.test(basename) ? `Svg${basename}` : basename;
     return `export { default as ${exportName}Icon } from './${basename}'`;
   });
   exportEntries.push(
     `export type { IconProps } from './${path.basename(
-      filePaths[0],
-      path.extname(filePaths[0])
+      filePaths[0].path,
+      path.extname(filePaths[0].path)
     )}'`
   );
   return exportEntries.join("\n");
