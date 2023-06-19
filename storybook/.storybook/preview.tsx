50966df89119c2d303b893e8326e8e86bbe4f668
Kine Kai Moe Holthe <kine.holthe@sikt.no>
2023-06-19 14:11:07 +0200

    feat(storybook): add possibility to see components in dark/lightmode

--- storybook/.storybook/preview.tsx
+++ storybook/.storybook/preview.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import type { Preview } from "@storybook/react";
 import "../../packages/core/index.pcss";
 import "../../packages/icons/icon.pcss";
@@ -6,6 +7,33 @@ import "../../packages/button/button-link.pcss";
 import "./style.css";
 
 const preview: Preview = {
+  globalTypes: {
+    scheme: {
+      name: "Scheme",
+      description: "Select light or dark theme",
+      defaultValue: "",
+      toolbar: {
+        items: [
+          { value: "", icon: "mirror", title: "Color scheme" },
+          { value: "light", icon: "circlehollow", title: "Light mode" },
+          { value: "dark", icon: "circle", title: "Dark mode" },
+        ],
+        dynamicTitle: true,
+      },
+    },
+  },
+  decorators: [
+    (Story, context) => {
+      const scheme = context.globals;
+      const rootElement = document.querySelector("html");
+
+      if (rootElement !== null) {
+        rootElement.setAttribute("data-color-scheme", scheme.scheme);
+      }
+
+      return <Story />;
+    },
+  ],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
