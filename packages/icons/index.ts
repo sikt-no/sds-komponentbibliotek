cd237ddc35eb438312178438140c7784f57e5bd6
Kine Kai Moe Holthe <kine.holthe@sikt.no>
2023-08-03 12:46:49 +0200

    feat(icons): add LinkedinLogo and XLogo

--- packages/icons/index.ts
+++ packages/icons/index.ts
@@ -1,3 +1,4 @@
 export { SpinnerIcon } from "./SpinnerIcon";
+export type { ThirdPartyIconsProps } from "./ThirdPartyIcons";
 export * from "./build/index";
 import "./icon.pcss";
--- packages/icons/stories/Icon.stories.tsx
+++ packages/icons/stories/Icon.stories.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Meta, StoryObj } from "@storybook/react";
-import { IconProps, ArrowRightIcon, SpinnerIcon } from "../index";
+import {
+  IconProps,
+  ArrowRightIcon,
+  SpinnerIcon,
+  ThirdPartyIconsProps,
+} from "../index";
+import { LinkedInLogo, XLogo } from "../ThirdPartyIcons";
 
 const meta: Meta = {
   title: "Components/Icons/Icon",
@@ -9,7 +15,7 @@ const meta: Meta = {
 
 export default meta;
 
-type Story = StoryObj<IconProps>;
+type Story = StoryObj<IconProps | ThirdPartyIconsProps>;
 
 export const ArrowRight: Story = {
   render: (args) => (
@@ -23,4 +29,28 @@ export const Spinner: Story = {
     <SpinnerIcon {...args} style={{ fontSize: "calc(32rem / 16)" }} />
   ),
   args: {},
+};
+
+export const Linkedin: Story = {
+  argTypes: {
+    color: {
+      options: ["white", "black"],
+      control: { type: "select" },
+    },
+  },
+  render: (args) => (
+    <LinkedInLogo style={{ fontSize: "calc(32rem / 16)" }} {...args} />
+  ),
+};
+
+export const XLogoIcon: Story = {
+  argTypes: {
+    color: {
+      options: ["white", "black"],
+      control: { type: "select" },
+    },
+  },
+  render: (args) => (
+    <XLogo style={{ fontSize: "calc(32rem / 16)" }} {...args} />
+  ),
 };
