1d6693a2bf6ef79197fbdcd70543d8336cefcebf
Kristoffer Nordström <totte.nordstrom@gmail.com>
2023-11-08 16:19:27 +0100

    feat(gatsby): add hero component

---
+++ gatsby/src/components/Hero.tsx
@@ -0,0 +1,62 @@
+import React, { ReactNode } from "react";
+import clsx from "clsx";
+import { BreadcrumbItem, Breadcrumbs } from "@sikt/sds-breadcrumbs";
+import { Link as GatsbyLink } from "gatsby-link";
+import { Heading1 } from "@sikt/sds-core";
+import * as style from "./hero.module.css";
+
+interface BreadcrumbProps {
+  to: string;
+  title: string;
+}
+
+interface HeroProps {
+  className?: string;
+  heading: ReactNode;
+  leadParagraph?: string;
+  breadcrumbs?: BreadcrumbProps[];
+  imgAlt?: string;
+  imgSrc?: string;
+}
+
+export const Hero = ({
+  className,
+  breadcrumbs,
+  heading,
+  leadParagraph,
+  imgAlt,
+  imgSrc,
+}: HeroProps) => {
+  return (
+    <>
+      <section className={clsx(style.hero__section, className)}>
+        <div className={style.hero__sectionContent}>
+          {breadcrumbs && (
+            <Breadcrumbs aria-label="Navigasjonssti">
+              {breadcrumbs.map((breadcrumb) => (
+                <BreadcrumbItem>
+                  <GatsbyLink
+                    className="sds-typography-link"
+                    to={breadcrumb.to}
+                  >
+                    {breadcrumb.title}
+                  </GatsbyLink>
+                </BreadcrumbItem>
+              ))}
+            </Breadcrumbs>
+          )}
+          <Heading1 variant="huge">{heading}</Heading1>
+          {leadParagraph && (
+            <p className={style.hero__paragraph}>{leadParagraph}</p>
+          )}
+        </div>
+
+        {imgSrc && imgAlt && (
+          <div className={style.hero__sectionImage}>
+            <img src={imgSrc} alt={imgAlt} />
+          </div>
+        )}
+      </section>
+    </>
+  );
+};
