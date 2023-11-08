226dfd1b4ba6c82e696bd41842bb06663fe9ee74
Kristoffer Nordström <totte.nordstrom@gmail.com>
2023-11-08 21:56:57 +0100

    feat(stylelint-config): override config for css modules

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -16,4 +16,49 @@ module.exports = {
       },
     ],
   },
+  overrides: [
+    {
+      files: ["**/*.module.css"],
+      rules: {
+        "selector-class-pattern": "",
+        "selector-pseudo-class-no-unknown": [
+          true,
+          {
+            ignorePseudoClasses: [
+              "export",
+              "import",
+              "global",
+              "local",
+              "external",
+            ],
+          },
+        ],
+        "selector-type-no-unknown": [
+          true,
+          {
+            ignoreTypes: ["from"],
+          },
+        ],
+        "property-no-unknown": [
+          true,
+          {
+            ignoreProperties: ["composes", "compose-with"],
+            ignoreSelectors: [":export", /^:import/],
+          },
+        ],
+        "at-rule-no-unknown": [
+          true,
+          {
+            ignoreAtRules: ["value"],
+          },
+        ],
+        "function-no-unknown": [
+          true,
+          {
+            ignoreFunctions: ["global"],
+          },
+        ],
+      },
+    },
+  ],
 };
