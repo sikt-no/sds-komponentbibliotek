55b4c0a852e3fe9bcdb027b53b7908cffbf60c04
Sondre Løvhaug <sondre.lovhaug@sikt.no>
2024-01-15 19:12:28 +0100

    test(checkbox): add playwright tests for checkbox with snapshots

---
+++ packages/checkbox/playwright/CheckboxInput.spec.ts
@@ -0,0 +1,88 @@
+import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
+
+test.describe("Checkbox", () => {
+  const componentSelector = ".sds-form-fieldset";
+
+  test.describe("a11y", () => {
+    test("checkbox fieldset with legend should be accessible", async ({
+      page,
+    }) => {
+      await page.goto(
+        "/iframe.html?viewMode=story&id=components-checkbox-checkboxfieldset--with-legend",
+      );
+
+      await page.locator(componentSelector).waitFor();
+
+      const accessibilityScanResults = await new AxeBuilder({ page })
+        .include(componentSelector)
+        .analyze();
+
+      expect(accessibilityScanResults.violations).toEqual([]);
+    });
+
+    test("checkbox fieldset with help text should be accessible", async ({
+      page,
+    }) => {
+      await page.goto(
+        "/iframe.html?viewMode=story&id=components-checkbox-checkboxfieldset--with-help-text",
+      );
+
+      await page.locator(componentSelector).waitFor();
+
+      const accessibilityScanResults = await new AxeBuilder({ page })
+        .include(componentSelector)
+        .analyze();
+
+      expect(accessibilityScanResults.violations).toEqual([]);
+    });
+
+    test("checkbox fieldset with error should be accessible", async ({
+      page,
+    }) => {
+      await page.goto(
+        "/iframe.html?viewMode=story&id=components-checkbox-checkboxfieldset--with-error",
+      );
+
+      await page.locator(componentSelector).waitFor();
+
+      const accessibilityScanResults = await new AxeBuilder({ page })
+        .include(componentSelector)
+        .analyze();
+
+      expect(accessibilityScanResults.violations).toEqual([]);
+    });
+  });
+
+  test.describe("visual", () => {
+    test("checkbox fieldset with legend should have screenshot", async ({
+      page,
+    }) => {
+      await page.goto(
+        "/iframe.html?viewMode=story&id=components-checkbox-checkboxfieldset--with-legend",
+      );
+
+      await expect(page.locator(componentSelector)).toHaveScreenshot();
+    });
+
+    test("checkbox fieldset with help text should have screenshot", async ({
+      page,
+    }) => {
+      await page.goto(
+        "/iframe.html?viewMode=story&id=components-checkbox-checkboxfieldset--with-help-text",
+      );
+
+      await expect(page.locator(componentSelector)).toHaveScreenshot();
+    });
+
+    test("checkbox fieldset with error should have screenshot", async ({
+      page,
+    }) => {
+      await page.goto(
+        "/iframe.html?viewMode=story&id=components-checkbox-checkboxfieldset--with-error",
+      );
+
+      await expect(page.locator(componentSelector)).toHaveScreenshot();
+    });
+  });
+});
