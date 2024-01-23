7f173e0a610c0f0f59f73145436dc3dede9609b0
Sondre Lovhaug <sondre.lovhaug@gmail.com>
2024-01-23 15:11:49 +0100

    test(pagination): add playwright test with snapshots

---
+++ packages/pagination/playwright/Pagination.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
+
+test.describe("Pagination", () => {
+  const componentSelector = ".sds-pagination";
+
+  test.describe("a11y", () => {
+    test("pagination should be accessible", async ({ page }) => {
+      await page.goto(
+        "/iframe.html?viewMode=story&id=components-pagination--default",
+      );
+
+      await page.locator(componentSelector).waitFor();
+
+      const accessibilityScanResults = await new AxeBuilder({ page })
+        .include(componentSelector)
+        .analyze();
+
+      expect(accessibilityScanResults.violations).toEqual([]);
+    });
+  });
+
+  test.describe("visual", () => {
+    test("pagination should have screenshot", async ({ page }) => {
+      await page.goto(
+        "/iframe.html?viewMode=story&id=components-pagination--default",
+      );
+
+      await expect(page.locator(componentSelector)).toHaveScreenshot();
+    });
+  });
+});
