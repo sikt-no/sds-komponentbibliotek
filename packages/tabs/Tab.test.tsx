9af2e8321b31babaf34ea1bf8af8a47cb4497ef5
Kristoffer Nordström <totte.nordstrom@gmail.com>
2024-02-26 10:35:59 +0100

    test(tabs): add missing test for context provider check

--- packages/tabs/Tab.test.tsx
+++ packages/tabs/Tab.test.tsx
@@ -1,15 +1,15 @@
 import { render, screen } from "@testing-library/react";
-import { TabPanel } from "./TabPanel";
+import { Tab } from "./Tab";
 
-describe("TabPanel", () => {
+describe("Tab", () => {
   describe("api", () => {
     it("should not render if outside context provider", async () => {
       const spy = jest
         .spyOn(global.console, "warn")
         .mockImplementationOnce(() => jest.fn);
-      render(<TabPanel>Content 1</TabPanel>);
+      render(<Tab>Tab 1</Tab>);
 
-      expect(screen.queryByText("Content 1")).toBeNull();
+      expect(screen.queryByText("Tab 1")).toBeNull();
       expect(spy).toHaveBeenCalled();
     });
   });
