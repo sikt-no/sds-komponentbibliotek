08ab338c93eed6f306963c079acf2257d1b90d4d
Kristoffer Nordström <totte.nordstrom@gmail.com>
2024-09-09 12:06:08 +0200

    test(badge): remove duplicate tests

--- packages/badge/playwright/Badge.spec.ts
+++ packages/badge/playwright/Badge.spec.ts
@@ -7,34 +7,6 @@ test.describe("Badge", () => {
   const visibilities = ["strong", "subtle"];
 
   test.describe("a11y", () => {
-    test("default badge should be accessible", async ({ page }) => {
-      await page.goto(
-        "/iframe.html?viewMode=story&id=components-badge--default",
-      );
-
-      await page.locator(componentSelector).waitFor();
-
-      const accessibilityScanResults = await new AxeBuilder({ page })
-        .include(componentSelector)
-        .analyze();
-
-      expect(accessibilityScanResults.violations).toEqual([]);
-    });
-
-    test("icon left badge should be accessible", async ({ page }) => {
-      await page.goto(
-        "/iframe.html?viewMode=story&id=components-badge--icon-left",
-      );
-
-      await page.locator(componentSelector).waitFor();
-
-      const accessibilityScanResults = await new AxeBuilder({ page })
-        .include(componentSelector)
-        .analyze();
-
-      expect(accessibilityScanResults.violations).toEqual([]);
-    });
-
     variants.map((variant) => {
       visibilities.map((visibility) => {
         test(`${variant} with visibility ${visibility} should be accessible`, async ({
@@ -54,25 +26,23 @@ test.describe("Badge", () => {
         });
       });
     });
-  });
 
-  test.describe("visual", () => {
-    test("default badge should have screenshot", async ({ page }) => {
-      await page.goto(
-        "/iframe.html?viewMode=story&id=components-badge--default",
-      );
-
-      await expect(page.locator(componentSelector)).toHaveScreenshot();
-    });
-
-    test("icon left badge should have screenshot", async ({ page }) => {
+    test("icon left badge should be accessible", async ({ page }) => {
       await page.goto(
         "/iframe.html?viewMode=story&id=components-badge--icon-left",
       );
 
-      await expect(page.locator(componentSelector)).toHaveScreenshot();
-    });
+      await page.locator(componentSelector).waitFor();
 
+      const accessibilityScanResults = await new AxeBuilder({ page })
+        .include(componentSelector)
+        .analyze();
+
+      expect(accessibilityScanResults.violations).toEqual([]);
+    });
+  });
+
+  test.describe("visual", () => {
     variants.map((variant) => {
       visibilities.map((visibility) => {
         test(`${variant} with visibility ${visibility} should have screenshot`, async ({
@@ -85,6 +55,14 @@ test.describe("Badge", () => {
           await expect(page.locator(componentSelector)).toHaveScreenshot();
         });
       });
+    });
+
+    test("icon left badge should have screenshot", async ({ page }) => {
+      await page.goto(
+        "/iframe.html?viewMode=story&id=components-badge--icon-left",
+      );
+
+      await expect(page.locator(componentSelector)).toHaveScreenshot();
     });
   });
 });
