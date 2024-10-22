073b07e3f2dfdba10e0f7052d21bdfb1e116c802
Kristoffer Nordström <totte.nordstrom@gmail.com>
2024-10-22 12:39:32 +0200

    ci(figma): add code connect publish to pipeline

---
+++ turbo/generators/template/figma/{{pascalCase name}}.figma.tsx
@@ -0,0 +1,12 @@
+import figma from "@figma/code-connect";
+import { {{pascalCase name}} } from "@sikt/sds-{{kebabCase name}}";
+
+figma.connect(
+  {{pascalCase name}},
+  "",
+  {
+    props: {},
+    example: () => (),
+    links: [],
+  },
+);
