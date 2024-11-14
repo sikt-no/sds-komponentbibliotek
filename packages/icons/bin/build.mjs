c007dfd090615a4ff111ad5626211534407ee3d2
Kristoffer Nordström <totte.nordstrom@gmail.com>
2024-11-14 09:21:33 +0100

    feat(icons)!: rename icons from visual representation to use representation

--- packages/icons/bin/build.mjs
+++ packages/icons/bin/build.mjs
@@ -4,19 +4,27 @@ import { config } from "../src/icons.config.mjs";
 let icons = [];
 icons.push(
   ...config
-    .filter((icon) => !icon.endsWith("-fill"))
+    .filter(({ id }) => !id.endsWith("-fill"))
     .map(
-      (icon) =>
-        `../../node_modules/@phosphor-icons/core/assets/regular/${icon}.svg`,
+      ({ id }) =>
+        `../../node_modules/@phosphor-icons/core/assets/regular/${id}.svg`,
     ),
 );
 icons.push(
   ...config
-    .filter((icon) => icon.endsWith("-fill"))
+    .filter(({ id }) => id.endsWith("-fill"))
     .map(
-      (icon) =>
-        `../../node_modules/@phosphor-icons/core/assets/fill/${icon}.svg`,
+      ({ id }) =>
+        `../../node_modules/@phosphor-icons/core/assets/fill/${id}.svg`,
     ),
 );
 
-cpy(icons, "dist/assets", { flat: true });
+cpy(icons, "dist/assets", {
+  flat: true,
+  rename: (basename) => {
+    const id = basename.split(".")[0];
+    const ext = basename.split(".")[1];
+    const icon = config.find((icon) => icon.id === id);
+    return `${icon.name}.${ext}`;
+  },
+});
