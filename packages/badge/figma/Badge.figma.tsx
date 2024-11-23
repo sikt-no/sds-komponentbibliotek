ac8dd566575a2cd9c0d870e3487555594653a17a
Kristoffer Nordström <totte.nordstrom@gmail.com>
2024-11-23 22:14:39 +0100

    docs(figma): add code connect

---
+++ packages/badge/figma/Badge.figma.tsx
@@ -0,0 +1,35 @@
+import figma from "@figma/code-connect";
+import { Badge } from "@sikt/sds-badge";
+
+figma.connect(
+  Badge,
+  "https://www.figma.com/design/RMhyuuEhXZ4vbKVrLQr4t4/SDS-Komponentbibliotek-2.0.0?node-id=1025-6572",
+  {
+    props: {
+      children: figma.string("Label"),
+      icon: figma.instance("Icon"),
+      variant: figma.enum("Color", {
+        Action: "primary",
+        Success: "success",
+        Danger: "critical",
+        Warning: "warning",
+        Info: "info",
+      }),
+      visibility: figma.enum("Visibility", {
+        High: "strong",
+        Medium: "subtle",
+      }),
+    },
+    example: ({ children, icon, variant, visibility }) => (
+      <Badge icon={icon} variant={variant} visibility={visibility}>
+        {children}
+      </Badge>
+    ),
+    links: [
+      {
+        name: "Storybook",
+        url: "https://designsystem.sikt.no/storybook/?path=/docs/components-badge-readme--docs",
+      },
+    ],
+  },
+);
