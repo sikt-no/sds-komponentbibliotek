050257cc390e0471e9ddee364c9437fc563fc8d3
Kristoffer Nordström <totte.nordstrom@gmail.com>
2024-11-24 21:48:00 +0100

    chore(figma): update template

--- turbo/generators/template/figma/{{pascalCase name}}.figma.tsx
+++ turbo/generators/template/figma/{{pascalCase name}}.figma.tsx
@@ -3,10 +3,15 @@ import { {{pascalCase name}} } from "@sikt/sds-{{kebabCase name}}";
 
 figma.connect(
   {{pascalCase name}},
-  "",
+  "https://www.figma.com/design/RMhyuuEhXZ4vbKVrLQr4t4/SDS-Komponentbibliotek-2.0.0?node-id=<node-id>",
   {
     props: {},
-    example: () => (),
-    links: [],
+    example: ({}) => (<{{pascalCase name}} />),
+    links: [
+      {
+        name: "Storybook",
+        url: "https://designsystem.sikt.no/storybook/?path=/docs/components-{{kebabCase name}}-readme--docs",
+      },
+    ],
   },
 );
