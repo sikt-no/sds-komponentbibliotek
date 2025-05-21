47e285610d815da57da14dfb64ec9f2cc45eac8a
Kristoffer Nordström <kristoffer.nordstrom@sikt.no>
2025-05-21 13:14:21 +0200

    fix(filter-list): export props

--- packages/filter-list/index.ts
+++ packages/filter-list/index.ts
@@ -1,5 +1,7 @@
 export type { FilterListProps } from "./src/FilterList/FilterList";
 export type { FilterListItemProps } from "./src/FilterListItem/FilterListItem";
+export type { FilterListCategoryProps } from "./src/FilterListCategory/FilterListCategory";
+export type { FilterListSectionProps } from "./src/FilterListSection/FilterListSection";
 export { FilterList } from "./src/FilterList/FilterList";
 export { FilterListItem } from "./src/FilterListItem/FilterListItem";
 export { FilterListCategory } from "./src/FilterListCategory/FilterListCategory";
