2bb006d4acb4b0c84b083b559cdbf996fc7f2c99
Kristoffer Nordström <kristoffer.nordstrom@sikt.no>
2025-11-03 10:18:27 +0100

    test(table): add playwright tests

---
+++ packages/table/playwright/Table.spec.ts
@@ -0,0 +1,32 @@
+import AxeBuilder from "@axe-core/playwright";
+import { test, expect } from "@playwright/test";
+
+test.describe("Table", () => {
+  const componentSelector = ".sds-table";
+
+  test.describe("a11y", () => {
+    test("should be accessible", async ({ page }) => {
+      await page.goto(
+        "/iframe.html?viewMode=story&id=components-table--default",
+      );
+
+      await page.locator(componentSelector).waitFor();
+
+      const accessibilityScanResults = await new AxeBuilder({ page })
+        .include(componentSelector)
+        .analyze();
+
+      expect(accessibilityScanResults.violations).toEqual([]);
+    });
+  });
+
+  test.describe("visual", () => {
+    test("should have screenshot", async ({ page }) => {
+      await page.goto(
+        "/iframe.html?viewMode=story&id=components-table--default",
+      );
+
+      await expect(page.locator(componentSelector)).toHaveScreenshot();
+    });
+  });
+});
