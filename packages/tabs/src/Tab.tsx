8a80e4044bbe4efc778e228366a30743cbf507d6
Kristoffer Nordström <kristoffer.nordstrom@sikt.no>
2025-11-17 08:42:11 +0100

    feat(tabs)!: add forwardref and onClick to tab component

--- packages/tabs/src/Tab.tsx
+++ packages/tabs/src/Tab.tsx
@@ -9,6 +9,8 @@ import {
   isValidElement,
   useContext,
   PropsWithChildren,
+  MouseEvent,
+  forwardRef,
 } from "react";
 import { TabsContext } from "./Tabs";
 
@@ -17,104 +19,113 @@ export interface TabProps extends HTMLAttributes<HTMLButtonElement> {
   className?: string;
   icon?: ReactNode;
   badge?: ReactNode;
+  onClick?: (e: MouseEvent) => void;
 }
 
-export const Tab = ({
-  children,
-  className,
-  icon,
-  badge,
-  ...rest
-}: TabProps) => {
-  const { index } = rest as { index: number };
-  const context = useContext(TabsContext);
+export const Tab = forwardRef<HTMLButtonElement, TabProps>(
+  ({ children, className, icon, badge, onClick, ...rest }: TabProps, ref) => {
+    const { index } = rest as { index: number };
+    const context = useContext(TabsContext);
 
-  if (!context) {
-    console.warn(
-      "TabContext missing. Component needs to be used inside <Tabs>",
-    );
-    return null;
-  }
-
-  const {
-    id,
-    count,
-    isSelectOnFocus,
-    selectedIndex,
-    setSelectedIndex,
-    setPreviousIndex,
-  } = context;
-
-  const isSelected = index === selectedIndex;
-  const handleSelect = (index: number) => {
-    if (isSelectOnFocus) {
-      setPreviousIndex(selectedIndex);
-      setSelectedIndex(index);
+    if (!context) {
+      console.warn(
+        "TabContext missing. Component needs to be used inside <Tabs>",
+      );
+      return null;
     }
-  };
-  const handleKeyPress = (event: KeyboardEvent<HTMLButtonElement>) => {
-    const firstIndex = 0;
-    const lastIndex = count - 1;
-    const currentTarget = event.currentTarget;
-    const firstChild = currentTarget.parentElement?.firstChild as HTMLElement;
-    const lastChild = currentTarget.parentElement?.lastChild as HTMLElement;
 
-    if (event.key === "ArrowLeft") {
-      const previousSibling = currentTarget.previousSibling as HTMLElement;
-      if (index > firstIndex) {
-        previousSibling.focus();
-      } else {
-        lastChild.focus();
-      }
+    const {
+      id,
+      count,
+      isSelectOnFocus,
+      selectedIndex,
+      setSelectedIndex,
+      setPreviousIndex,
+    } = context;
 
-      handleSelect(selectedIndex > firstIndex ? selectedIndex - 1 : lastIndex);
-    } else if (event.key === "ArrowRight") {
-      const nextSibling = currentTarget.nextSibling as HTMLElement;
-      if (index < lastIndex) {
-        nextSibling.focus();
-      } else {
-        firstChild.focus();
-      }
-
-      handleSelect(selectedIndex < lastIndex ? selectedIndex + 1 : firstIndex);
-    } else if (event.key === "Home") {
-      firstChild.focus();
-      handleSelect(firstIndex);
-    } else if (event.key === "End") {
-      lastChild.focus();
-      handleSelect(lastIndex);
-    } else if (event.key === "Enter" || event.code === "Space") {
-      setSelectedIndex(index);
-    }
-  };
-
-  return (
-    <button
-      className={clsx("sds-tabs__tab", className)}
-      role="tab"
-      aria-selected={isSelected}
-      aria-controls={`panel-${id}-${index}`}
-      id={`tab-${id}-${index}`}
-      tabIndex={isSelected ? 0 : -1}
-      onClick={() => {
+    const isSelected = index === selectedIndex;
+    const handleSelect = (index: number) => {
+      if (isSelectOnFocus) {
         setPreviousIndex(selectedIndex);
         setSelectedIndex(index);
-      }}
-      onKeyDown={handleKeyPress}
-      {...rest}
-    >
-      {icon && <span className="sds-tabs__tab-icon">{icon}</span>}
-      {children}
-      {badge && (
-        <span className="sds-tabs__tab-badge">
-          {isValidElement(badge) &&
-            cloneElement(badge as ReactElement<PropsWithChildren<BadgeProps>>, {
-              visibility: "strong",
-            })}
-        </span>
-      )}
-    </button>
-  );
-};
+      }
+    };
+    const handleKeyPress = (event: KeyboardEvent<HTMLButtonElement>) => {
+      const firstIndex = 0;
+      const lastIndex = count - 1;
+      const currentTarget = event.currentTarget;
+      const firstChild = currentTarget.parentElement?.firstChild as HTMLElement;
+      const lastChild = currentTarget.parentElement?.lastChild as HTMLElement;
+
+      if (event.key === "ArrowLeft") {
+        const previousSibling = currentTarget.previousSibling as HTMLElement;
+        if (index > firstIndex) {
+          previousSibling.focus();
+        } else {
+          lastChild.focus();
+        }
+
+        handleSelect(
+          selectedIndex > firstIndex ? selectedIndex - 1 : lastIndex,
+        );
+      } else if (event.key === "ArrowRight") {
+        const nextSibling = currentTarget.nextSibling as HTMLElement;
+        if (index < lastIndex) {
+          nextSibling.focus();
+        } else {
+          firstChild.focus();
+        }
+
+        handleSelect(
+          selectedIndex < lastIndex ? selectedIndex + 1 : firstIndex,
+        );
+      } else if (event.key === "Home") {
+        firstChild.focus();
+        handleSelect(firstIndex);
+      } else if (event.key === "End") {
+        lastChild.focus();
+        handleSelect(lastIndex);
+      } else if (event.key === "Enter" || event.code === "Space") {
+        setSelectedIndex(index);
+      }
+    };
+
+    return (
+      <button
+        ref={ref}
+        className={clsx("sds-tabs__tab", className)}
+        role="tab"
+        aria-selected={isSelected}
+        aria-controls={`panel-${id}-${index}`}
+        id={`tab-${id}-${index}`}
+        tabIndex={isSelected ? 0 : -1}
+        onClick={(event) => {
+          setPreviousIndex(selectedIndex);
+          setSelectedIndex(index);
+
+          if (onClick) {
+            onClick(event);
+          }
+        }}
+        onKeyDown={handleKeyPress}
+        {...rest}
+      >
+        {icon && <span className="sds-tabs__tab-icon">{icon}</span>}
+        {children}
+        {badge && (
+          <span className="sds-tabs__tab-badge">
+            {isValidElement(badge) &&
+              cloneElement(
+                badge as ReactElement<PropsWithChildren<BadgeProps>>,
+                {
+                  visibility: "strong",
+                },
+              )}
+          </span>
+        )}
+      </button>
+    );
+  },
+);
 
 Tab.displayName = "Tab";
