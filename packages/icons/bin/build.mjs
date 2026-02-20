0e4864d6e220492df1ba3680b946d2b96660d430
Vegard Svendsen <vegard.svendsen@sikt.no>
2026-02-20 12:53:56 +0100

    chore(icons): update cpy rename callback to v13 format

--- packages/icons/bin/build.mjs
+++ packages/icons/bin/build.mjs
@@ -1,35 +1,21 @@
 import cpy from "cpy";
 import { config } from "../src/icons.config.mjs";
 
-let icons = [];
-icons.push(
-  ...config
-    .filter(({ id, source }) => !id.endsWith("-fill") && source !== "sds")
-    .map(
-      ({ id }) =>
-        `../../node_modules/@phosphor-icons/core/assets/regular/${id}.svg`,
-    ),
-);
-icons.push(
-  ...config
-    .filter(({ id, source }) => id.endsWith("-fill") && source !== "sds")
-    .map(
-      ({ id }) =>
-        `../../node_modules/@phosphor-icons/core/assets/fill/${id}.svg`,
-    ),
-);
-icons.push(
-  ...config
-    .filter(({ source }) => source === "sds")
-    .map(({ id }) => `src/svg/${id}.svg`),
-);
+// Build all icon file paths based on their source and type
+const icons = config.map(({ id, source }) => {
+  if (source === "sds") return `src/svg/${id}.svg`;
 
-cpy(icons, "dist/assets", {
+  return id.endsWith("-fill")
+    ? `../../node_modules/@phosphor-icons/core/assets/fill/${id}.svg`
+    : `../../node_modules/@phosphor-icons/core/assets/regular/${id}.svg`;
+});
+
+// Copy all icons to dist/assets and rename them according to config
+await cpy(icons, "dist/assets", {
   flat: true,
-  rename: (basename) => {
-    const id = basename.split(".")[0];
-    const ext = basename.split(".")[1];
+  rename: (source, destination) => {
+    const id = source.nameWithoutExtension;
     const icon = config.find((icon) => icon.id === id);
-    return `${icon.name}.${ext}`;
+    destination.nameWithoutExtension = icon.name;
   },
 });
