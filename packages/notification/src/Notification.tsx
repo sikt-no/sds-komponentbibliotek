41c29e801f7f0c1ccb87980a5a534792fb1a29cf
Kristoffer Nordström <kristoffer.nordstrom@sikt.no>
2026-02-27 08:30:47 +0100

    feat(notification): add variant prop

--- packages/notification/src/Notification.tsx
+++ packages/notification/src/Notification.tsx
@@ -3,19 +3,28 @@ import { HTMLAttributes } from "react";
 import "./notification.pcss";
 
 export interface NotificationProps extends HTMLAttributes<HTMLSpanElement> {
+  variant?: "brand" | "neutral" | "success" | "info" | "warning" | "critical";
   className?: string;
   count?: number;
   maxCount?: number;
 }
 
 export const Notification = ({
+  variant = "info",
   className,
   count,
   maxCount = Infinity,
   ...rest
 }: NotificationProps) => {
   return (
-    <span className={clsx("sds-notification", className)} {...rest}>
+    <span
+      className={clsx(
+        "sds-notification",
+        `sds-notification--${variant}`,
+        className,
+      )}
+      {...rest}
+    >
       {count && (
         <span className="sds-notification__count">
           {maxCount < count ? `${maxCount}+` : count}
