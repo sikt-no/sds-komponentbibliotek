bbcd1748730b41af9d4433537ad9de1648d7d00c
Kristoffer Nordström <kristoffer.nordstrom@sikt.no>
2026-08-14 14:21:44 +0200

    chore(sd3): add sd3 stylelint rules

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,16 +1,31 @@
 /** @type {import('stylelint').Config} */
 export default {
   extends: "./packages/stylelint-config/index",
-  rules: {
-    "selector-class-pattern": [
-      "^sds-(?<block>(?:[a-z][a-z0-9]*)(?:-[a-z0-9]+)*)(?<element>(?:__[a-z][a-z0-9]*(?:-[a-z0-9]+)*))?(?<modifier>(?:--[a-z][a-z0-9]*)(?:-[a-z0-9]+)*)?$",
-      {
-        resolveNestedSelectors: true,
-      },
-    ],
-    "no-descending-specificity": null,
-  },
+  rules: {},
   overrides: [
+    {
+      /* INFO: SDS BEM style */
+      files: ["packages/**/*.css"],
+      rules: {
+        "selector-class-pattern": [
+          "^sds-(?<block>(?:[a-z][a-z0-9]*)(?:-[a-z0-9]+)*)(?<element>(?:__[a-z][a-z0-9]*(?:-[a-z0-9]+)*))?(?<modifier>(?:--[a-z][a-z0-9]*)(?:-[a-z0-9]+)*)?$",
+          {
+            resolveNestedSelectors: true,
+          },
+        ],
+        "no-descending-specificity": null,
+      },
+    },
+    {
+      /* INFO: SD3 @scope style */
+      files: [
+        "packages/design-system/**/*.css",
+        "packages/design-tokens/**/*.css",
+      ],
+      rules: {
+        "selector-class-pattern": "",
+      },
+    },
     {
       files: ["**/*.module.css", "**/stories/*.css"],
       rules: {
