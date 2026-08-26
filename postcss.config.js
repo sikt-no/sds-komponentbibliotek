0ef9a0e01e8ebc67f4dba9eeeedd64f904635291
designsystem-renovatebot <group_3766_bot_96bc8c72848663d65e58cc356ffd479f@noreply.gitlab.sikt.no>
2026-08-26 09:29:24 +0200

    chore(deps): update postcss (major)

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
+import autoprefixer from "autoprefixer";
+import postcssNested from "postcss-nested";
+import postcssImport from "postcss-import";
+import postcssCustomMedia from "postcss-custom-media";
+
 /** @type {import('postcss-load-config').Config} */
-module.exports = {
-  plugins: [
-    require("autoprefixer"),
-    require("postcss-nested"),
-    require("postcss-import"),
-    require("postcss-custom-media"),
-  ],
+export default {
+  plugins: [autoprefixer, postcssNested, postcssImport, postcssCustomMedia],
 };
